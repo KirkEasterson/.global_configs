3365ff8c600f2ff05f2faf98008735cdb0fc2d05
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-25 21:48:03 +0200

    Added config for plymouth

---
+++ plymouth/install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# name of directory that this script is in, so it can be run from anywhere
+PLY_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# create the /etc/plymouth/ directory if it doesn't exist
+sudo mkdir -p /etc/plymouth/
+
+# copy the plymouth config
+sudo cp $PLY_DIR/plymouthd.conf /etc/plymouth/plymouthd.conf
