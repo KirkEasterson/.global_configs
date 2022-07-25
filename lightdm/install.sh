e75b17df55ad0b4cacb2f802a7fc54893e25f79f
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-25 19:25:24 +0200

    Modified lightdm script so it can be run from anywhere

--- lightdm/install.sh
+++ lightdm/install.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
-sudo cp $HOME/.global_configs/lightdm/wall.jpg /usr/share/pixmaps/wall.jpg
-sudo cp $HOME/.global_configs/lightdm/slick-greeter.conf /etc/lightdm/slick-greeter.conf
+# name of directory that this script is in, so it can be run from anywhere
+LIGHTDM_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+sudo cp $LIGHTDM_DIR/wall.jpg /usr/share/pixmaps/wall.jpg
+sudo cp $LIGHTDM_DIR/slick-greeter.conf /etc/lightdm/slick-greeter.conf
