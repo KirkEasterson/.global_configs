d453065da06d7c4f7d267c85262fb7bb72be3c25
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-25 19:20:49 +0200

    Added global install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# name of directory that this script is in, so it can be run from anywhere
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# iterate through all directories, and run install.sh if it exists
+for dir in $SCRIPT_DIR/*/; do
+	if [ -f "$dir/install.sh" ]; then
+		source "$dir/install.sh"
+	fi
+done
