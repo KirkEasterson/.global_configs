ee1d379083ebccc5ac2e41c37dee1bf7b8ee0104
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-25 20:40:20 +0200

    Changed lightdm install script to make directory if it doesn't exist

--- lightdm/install.sh
+++ lightdm/install.sh
@@ -3,5 +3,11 @@
 # name of directory that this script is in, so it can be run from anywhere
 LIGHTDM_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# copy greeter wallpaper
 sudo cp $LIGHTDM_DIR/wall.jpg /usr/share/pixmaps/wall.jpg
+
+# create the /etc/lightdm/ dir if ti doesn't exist
+sudo mkdir -p /etc/lightdm/
+
+# copy the slick greeter config
 sudo cp $LIGHTDM_DIR/slick-greeter.conf /etc/lightdm/slick-greeter.conf
