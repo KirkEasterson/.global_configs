95a21f7e4dff850cd3a66c58a6eab6116ce7a00a
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-26 17:26:47 +0200

    Fixed issue where LLS install couldn't be run from any directory

--- lua/install.sh
+++ lua/install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+# name of directory that this script is in, so it can be run from anywhere
+LLS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # fetch the latest version
 LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/sumneko/lua-language-server/releases/latest)
 LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 LATEST_FILE="lua-language-server-$LATEST_VERSION-linux-x64.tar.gz"
 ARTIFACT_URL="https://github.com/sumneko/lua-language-server/releases/download/$LATEST_VERSION/$LATEST_FILE"
-wget -O $LATEST_FILE $ARTIFACT_URL
+wget -O $LLS_DIR/$LATEST_FILE $ARTIFACT_URL
 
 # extract the download
-mkdir -p lua-language-server-bin
-tar -xf $LATEST_FILE -C lua-language-server-bin/
-rm $LATEST_FILE
+mkdir -p $LLS_DIR/lua-language-server-bin
+tar -xf $LLS_DIR/$LATEST_FILE -C $LLS_DIR/lua-language-server-bin/
+rm $LLS_DIR/$LATEST_FILE
 
 # move the files to /bin/
 sudo rm -rf /bin/lua-language-server-bin
-sudo mv -f lua-language-server-bin /bin/
-sudo cp lua-language-server /bin/
+sudo mv -f $LLS_DIR/lua-language-server-bin /bin/
+sudo cp $LLS_DIR/lua-language-server /bin/
